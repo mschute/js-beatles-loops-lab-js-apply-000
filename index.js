1d3b16d0b70781a420c045a1913636d78cd9574d
Marta Unterschute <marta.unterschute@gmail.com>
2019-01-21 23:19:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+  for (let i = 0; i < musicians.length; i++){
+   emptyArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return emptyArray
+}  
+
+function johnLennonFacts(facts){
+  let i = 0
+  while(i < facts.length){
+    facts.push([i] + '!!!');
+    i++;
+  }
+}
