f12e2c116b2a94466b2634560b612a231d98f802
Marta Unterschute <marta.unterschute@gmail.com>
2019-01-21 23:26:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(number){
   var loveArray = [];
-  do {'I love the Beatles!'}
+  do {'I love the Beatles!'; i++}
   while 
   loveArray.push()
   return loveArray
